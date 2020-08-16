912860a93d18ee67e04bf9dd4b145c9eae1fcecd
hasanuzzaman.dev@gmail.com <hasanuzzaman.dev@gmail.com>
2020-08-16 12:52:18 +0600

    add NavController to name in toolbar

--- app/src/main/java/com/hasan/shoppingcart/views/activity/MainActivity.java
+++ app/src/main/java/com/hasan/shoppingcart/views/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hasan.shoppingcart.views.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -8,9 +11,15 @@ import com.hasan.shoppingcart.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
     }
 }
