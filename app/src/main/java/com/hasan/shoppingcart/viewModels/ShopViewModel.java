8d3c3fa307a35d215a4c9f070b26761b8afac3e9
Hasan <hasanuzzaman.dev@gmail.com>
2020-08-30 22:52:38 +0600

    add mutableLiveData in ShopViewModel

--- app/src/main/java/com/hasan/shoppingcart/viewModels/ShopViewModel.java
+++ app/src/main/java/com/hasan/shoppingcart/viewModels/ShopViewModel.java
@@ -1,6 +1,7 @@
 package com.hasan.shoppingcart.viewModels;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.hasan.shoppingcart.models.Product;
@@ -12,7 +13,13 @@ public class ShopViewModel  extends ViewModel {
 
     ShopRepo shopRepo = new ShopRepo();
 
+    MutableLiveData<Product> productMutableLiveData = new MutableLiveData<>();
+
     public LiveData<List<Product>> getProducts(){
         return shopRepo.getProducts();
+    }
+
+    public void setProduct(Product product){
+        productMutableLiveData.setValue(product);
     }
 }
