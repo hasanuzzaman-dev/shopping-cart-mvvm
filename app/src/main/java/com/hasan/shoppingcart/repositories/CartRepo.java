5ab216f9dbfa1f9517a6331d38653d47778e89ab
Hasan <hasanuzzaman.dev@gmail.com>
2020-11-19 17:20:03 +0600

    merging item in cart

--- app/src/main/java/com/hasan/shoppingcart/repositories/CartRepo.java
+++ app/src/main/java/com/hasan/shoppingcart/repositories/CartRepo.java
@@ -16,8 +16,8 @@ public class CartRepo {
     private static final String TAG = "CartRepo";
     private MutableLiveData<List<CartItem>> mutableCart = new MutableLiveData<>();
 
-    public LiveData<List<CartItem>> getCart(){
-        if (mutableCart.getValue() == null){
+    public LiveData<List<CartItem>> getCart() {
+        if (mutableCart.getValue() == null) {
             initCart();
         }
         return mutableCart;
@@ -27,14 +27,31 @@ public class CartRepo {
         mutableCart.setValue(new ArrayList<CartItem>());
     }
 
-    public boolean addItemToCart(Product product){
-        if (mutableCart.getValue() == null){
+    public boolean addItemToCart(Product product) {
+        if (mutableCart.getValue() == null) {
             initCart();
         }
 
-        Log.d(TAG, "addItemToCart: "+mutableCart.getValue());
+        //Log.d(TAG, "addItemToCart: "+mutableCart.getValue());
 
         List<CartItem> cartItemList = new ArrayList<>(mutableCart.getValue());
+
+        // if quantity is greater then 1
+        for (CartItem cartItem : cartItemList) {
+            if (cartItem.getProduct().getId().equals(product.getId())) {
+                if (cartItem.getQuantity() == 5) {
+                    return false;
+                }
+
+                int index = cartItemList.indexOf(cartItem);
+                cartItem.setQuantity(cartItem.getQuantity() + 1);
+                cartItemList.set(index,cartItem);
+
+                mutableCart.setValue(cartItemList);
+
+                return true;
+            }
+        }
         CartItem cartItem = new CartItem(product, 1);
         cartItemList.add(cartItem);
         mutableCart.setValue(cartItemList);
